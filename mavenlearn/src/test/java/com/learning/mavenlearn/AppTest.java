f7525cc8c6838fc5cb6f0b7c93376535e0b27a06
chaithra1996 <114689888+chaithra1996@users.noreply.github.com>
2024-01-05 17:07:58 +0530

    Update AppTest.java

--- mavenlearn/src/test/java/com/learning/mavenlearn/AppTest.java
+++ mavenlearn/src/test/java/com/learning/mavenlearn/AppTest.java
@@ -10,6 +10,14 @@ public class AppTest
 {
 	public static void main(String[] args) {
 		WebDriver driver=new ChromeDriver();
-		driver.get("https://www.amazon.jobs/en/jobs/2503582/quality-assurance-engineer-amazon-smart-vehicles");
+		driver.get("https://online.actitime.com/cisco2");
+			driver.manage().window().maximize();
+			driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+			driver.findElement(By.id("username")).sendKeys("unique.muralistl@gmail.com");
+			driver.findElement(By.name("pwd")).sendKeys("DgjTEH99");
+			driver.findElement(By.cssSelector("input[name='remember']")).click();
+			driver.findElement(By.xpath("//a[@id='loginButton']")).click();
+			driver.findElement(By.linkText("Logout")).click();
+			driver.close()
 	}
 }
